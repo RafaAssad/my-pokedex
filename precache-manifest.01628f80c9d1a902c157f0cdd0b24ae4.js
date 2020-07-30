32faa116ed8418a7da59ceb084c44ad212f3fbc6
Rafael Assad <rafaassadeng@gmail.com>
2020-07-30 16:54:03 -0300

    Updates

---
+++ precache-manifest.01628f80c9d1a902c157f0cdd0b24ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a44de527969dfe21bfc06d43ee77884",
+    "url": "/my-pokedex/index.html"
+  },
+  {
+    "revision": "6f6ba4791deccbc39539",
+    "url": "/my-pokedex/static/css/main.b1d2aa9d.chunk.css"
+  },
+  {
+    "revision": "8b043fdb2f75809140f3",
+    "url": "/my-pokedex/static/js/2.814e85b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-pokedex/static/js/2.814e85b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6ba4791deccbc39539",
+    "url": "/my-pokedex/static/js/main.0fdaffb8.chunk.js"
+  },
+  {
+    "revision": "f0c54395f4d2b6baf565",
+    "url": "/my-pokedex/static/js/runtime-main.c6223624.js"
+  }
+]);
